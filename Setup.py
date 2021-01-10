53cdd3cab45c7fbea8fe7184003e25ee06cd1a1a
Abraham <43353411+Rezatachi@users.noreply.github.com>
2021-01-09 20:34:19 -0500

    First Commit

--- Setup.py
+++ Setup.py
@@ -1,16 +1,47 @@
 import discord
 import random
 import asyncio
-from discord.ext import commands
+from discord.ext import commands,tasks
 from discord.ext.commands import Bot, has_permissions, CheckFailure
+from itertools import cycle
+import os
 
 client = commands.Bot(command_prefix = ']')
+status = cycle(['NODES', 'MODULES', 'WAVES'])
 
+
+# Initalizes the bot and gives it a status
 @client.event 
 async def on_ready():
-    activity = discord.Game(name="Type #Bot for Info on Coden", description="Hi I'm a somewhat useful bot :D")
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="discord inquiries"))
+    change_status.start()
     print("Bot is online.")
+
+# Backgrounds tasks for the Discord Bot ()
+@tasks.loop(seconds=100000)
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(status)))
+
+# These commands load and Unload the cogs from ./cogs folder
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+    
+
+@client.command()
+async def unload(ctx, extension):
+     client.unload_extension(f'cogs.{extension}')
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+
+# Searches for the python scripts in the directory and attaches a string to them
+for filename in os.listdir('./cogs'):
+    if filename.endswith ('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+
 
 @client.event
 async def on_memberjoin(member):
@@ -43,13 +74,24 @@ async def ask(message):
     embedHelp.add_field(name = "Command and Intents", value= "`commands` - displays the help embed displaying the various commands\n  `info` - shows the bot information \n `clear` auto purges messages up to 10 \n `8ball` - fun minigame to test your luck\n  `ban`- bans are member from the discord server\n  `kick`- kicks a member from the server.")
     embedHelp.set_thumbnail(url="https://cdn.discordapp.com/attachments/761256129191477261/779902108920709141/Ten.jpg")
     await message.channel.send(embed=embedHelp)
+   
 
 @client.command()
-async def clear(ctx, amount=10):
+async def clear(ctx, amount : int):
     await ctx.channel.purge(limit=amount)
-    botmessage = await ctx.send(f'{amount} messages purged.')
+    embedClear = discord.Embed(title=f'**{amount} messages have been cleared!**')
+    botclear = await ctx.channel.send(embed=embedClear)
     await asyncio.sleep(1)
-    await botmessage.delete()
+    await botclear.delete()
+
+# Clear error displayed
+@clear.error
+async def clear_error(ctx, error):
+     if isinstance(error, commands.MissingRequiredArgument):
+       embedArguement = discord.Embed(title="**Please specify the amount of messages to delete**!")
+       botembedArguement = await ctx.channel.send(embed=embedArguement)
+       await asyncio.sleep(1)
+       await botembedArguement.delete()
     
 @client.command()
 @commands.has_permissions(kick_members=True)
@@ -57,7 +99,7 @@ async def kick(message, member : discord.Member, *, reason=None):
     await member.kick(reason=reason)
     embedVar2 = discord.Embed(title="**User kicked!**", color=0x8919cf)
     botembed = await message.channel.send(embed=embedVar2)
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     await botembed.delete()
 
   
@@ -67,7 +109,7 @@ async def ban(message, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
     embedVar2 = discord.Embed(title="**User banned!**", color=0x8919cf)
     botembedvar = await message.channel.send(embed=embedVar2)
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     await botembedvar.delete()
 
     
